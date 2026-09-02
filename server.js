cae21021fefb48627585ec11ab928443ae2fd458
Pari <2025.parip@isu.ac.in>
2026-09-02 10:45:58 +0530

    Update MongoDB connection success message for clarity

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log("MongoDB connected successfully");
+    console.log("MongoDB connected successfully!!!");
 
     app.listen(process.env.PORT, () => {
       console.log(`Server running on port ${process.env.PORT}`);
